6779e255d8aacc3249fdc5346fe72da2e485e730
Anthony WEBSTER <anthony.webster@ADIONICS.COM>
2025-07-03 16:57:38 +0200

    add tests for density,cclo4_eq_org,ctoy and ytoc

---
+++ tests/TestPerchloratesCommon.m
@@ -0,0 +1,39 @@
+classdef TestPerchloratesCommon < matlab.unittest.TestCase
+    %TestPerchloratesCommon Test Suite for common perchlorates
+    methods (Test)
+        function density(testCase)
+            rho = mvu(1e3,'kg/ L');
+            cc =  mvu([3;5],'mmol/ L');
+            Mc =  mvu([13;17],'kg/mmol');
+            ca =  mvu([7;11],'mmol/ L');
+            Ma =  mvu([19;23],'kg/mmol');
+            trial = perchlorates_density(rho,cc,ca,Mc,Ma);
+            truth = mvu(1e3 - (3*13+5*17+7*19+11*23),'kg_eau/ L');
+            testCase.verifyEqual(trial,truth)
+        end
+
+        function cclo4_eq_org(testCase)
+           zc = mvu([1,2,3],'');
+           cc = mvu([4,5,6],'mmol/ L');
+           trial = perchlorates_cclo4_eq_org(zc,cc);
+           truth = mvu(32,'mmol/ L');
+           testCase.verifyEqual(trial,truth)
+        end
+
+        function perchlorates_ctoy(testCase)
+            cs = mvu([8,4],'mmol/ L');
+            rho = mvu(2,'kg_eau/ L');
+            trial = perchlorates_ctoy(cs,rho);
+            truth = mvu([4,2],'mmol/kg_eau');
+            testCase.verifyEqual(trial,truth)
+        end
+
+        function perchlorates_ytoc(testCase)
+            ys = mvu([8,4],'mmol/kg_eau');
+            rho = mvu(2,'kg_eau/ L');
+            trial = perchlorates_ytoc(ys,rho);
+            truth = mvu([16,8],'mmol/ L');
+            testCase.verifyEqual(trial,truth)
+        end
+    end
+end
